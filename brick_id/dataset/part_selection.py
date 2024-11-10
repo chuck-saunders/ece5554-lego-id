07a7ab09cd50e446ca3fe1a5ceedf5b5ca383b90
Chuck Saunders <goband@gmail.com>
2024-11-10 13:18:37 -0500

    Made PDF files for each dataset to simplify finding parts for individual datasets

--- brick_id/dataset/part_selection.py
+++ brick_id/dataset/part_selection.py
@@ -33,6 +33,140 @@ class RandomizedDataset(object):
         for key, val in self.selected_part_counts.items():
             self.selected_parts.append((parts_list[key], val))
 
+
+# Trying to get text formatted in the PDF was a bear; apparently there's no way to control the line spacing on a word-
+# wrapped cell using fpdf2 so eventually I gave up and wrote this function to manually word-wrap.
+def wrap_text_to_column_width(text:str, width: int) -> list[str]:
+    if len(text) <= width:
+        return [text]
+    split_idx = width-1
+    if text[split_idx] != ' ':
+        while split_idx >= 0:
+            if text[split_idx] == ' ':
+                break
+            split_idx -= 1
+    if split_idx < 0:
+        raise Exception(f'Failed to find a space that allows formatting text {text} to column width {width}')
+    # We're on a space now, so keep everything up to that space:
+    result = [text[:split_idx]]
+    # Recursively call to split the rest:
+    remainder = wrap_text_to_column_width(text[split_idx+1:], width)
+    # Join what we've got:
+    for item in remainder:
+        result.append(item)
+    # Return the result
+    return result
+
+
+# The following code based on Google's Generative AI in response to the search "create a table with images and save to
+# pdf python" on 30 October 2024.
+
+class PDF(FPDF):
+    def imagex(self, image_path, x, y, w=0, h=0):
+        if w == 0 and h == 0:
+            img = Image.open(image_path)
+            w, h = img.size
+        self.image(image_path, x, y, w, h)
+
+def export_to_pdf(dataset_id, dataset: RandomizedDataset):
+    cwd = os.path.abspath(os.path.dirname(__file__))
+    imgs_path = cwd + '/imgs/'
+    pdf = PDF()
+    pdf.add_page()
+
+    # How big the pdf.get_y() value can get before we should add a page break
+    page_height = 260
+
+    # Set cell dimensions
+    img_width = 15
+    img_height = 15
+    description_width = 25
+    text_width = 10
+    text_height = 5
+
+    # Add table rows
+    pdf.set_font("Helvetica", "", 8)
+    # Fill color for alternating lines:
+    pdf.set_fill_color(225, 225, 225)
+    should_fill = False
+    lines = 0
+    for (part_number, description), count in dataset.selected_parts:
+        lines += 1
+        should_fill = lines % 2 == 1
+        starting_x = pdf.get_x()
+        starting_y = pdf.get_y()
+        # Okay, how TALL is this row? We know that:
+        # 1. Image height will be img_height,
+        # 2. Quantities will be broken into four rows, one for the dataset and one for the quantity, so 2*text_height per
+        #    test quantity, and then I want two tests stacked per row (because testing shows I can't fit all datasets
+        #    horizontally on one page), so 2*(2*text_height) total
+        # 3. The description. This uses the wrap_text_to_column_width method defined above, which uses description_width as
+        #    the width. This means we need to call the function, see how many lines there are, then multiply that by the
+        #    text_height.
+        #
+        # then take the max of all three above, and that's the row height.
+        description_rows = wrap_text_to_column_width(description, 15)
+        row_height = max([img_height, (2 * text_height), len(description_rows * text_height)])
+
+        # Do we need to add a new page?
+        if starting_y + row_height > page_height:
+            pdf.add_page()
+            starting_x = pdf.get_x()
+            starting_y = pdf.get_y()
+
+        # Add the image
+        img_path = imgs_path + part_number + '.png'
+        pdf.imagex(img_path, starting_x, starting_y, img_width, img_height)
+
+        # Adding the image only adds an image, it doesn't make a cell or affect the PDF document position. I think having
+        # the border helps readability of the document quite a bit, so I'll draw a box around the image that's padded out
+        # to the maximum height
+        #
+        # Don't fill the cell; the background gray doesn't look good with the white background for most cells.
+        pdf.cell(img_width, row_height, '', 1, fill=False)
+
+        current_x = starting_x + img_width
+
+        # Kinda similar to the image, I want the description to be printed in multiple lines without borders, so it looks
+        # like one cell, but then I get no borders. So, add one large cell with a border!
+        pdf.set_xy(current_x, starting_y)
+
+        pdf.cell(description_width, row_height, '', 1, fill=should_fill)
+        pdf.set_xy(current_x, starting_y)
+
+        # Print the part description
+        for i in range(len(description_rows)):
+            line = description_rows[i]
+            pdf.set_y(starting_y + i * text_height)
+            pdf.set_x(current_x)
+            pdf.cell(description_width, text_height, line, fill=False)
+        current_x += description_width
+
+        # Print min and ideal quantities
+        pdf.set_xy(current_x, starting_y)
+        pdf.cell(text_width, row_height, '', 1, fill=should_fill)
+        pdf.set_xy(current_x, starting_y)
+        pdf.cell(text_width, text_height, 'Need', fill=should_fill)
+        pdf.set_xy(current_x, starting_y + text_height)
+        pdf.cell(text_width, text_height, str(count), fill=should_fill)
+        # Go over the cell one more time to get the border to look nice
+        pdf.set_xy(current_x, starting_y)
+        pdf.cell(text_width, row_height, '', 1, fill=False)
+
+        if lines == 1:
+            pdf.set_xy(current_x + text_width, starting_y)
+            lighting_text = 'Lighting condition: ' + str(dataset.lighting_condition)
+            background_text = 'Background condition ' + str(dataset.background_condition)
+            pdf.cell(5*text_width, text_height, lighting_text)
+            pdf.set_xy(current_x + text_width, starting_y + text_height)
+            pdf.cell(5*text_width, row_height, background_text)
+
+        # Advance!
+        pdf.set_x(starting_x)
+        pdf.set_y(starting_y + row_height)
+
+    pdf.output(cwd + '/dataset_' + str(dataset_id) + '.pdf')
+
 cwd = os.path.abspath(os.path.dirname(__file__))
 imgs_path = cwd + '/imgs/'
 
@@ -41,6 +175,7 @@ num_images = 25
 datasets = list()
 for i in range(num_images):
     datasets.append(RandomizedDataset())
+    export_to_pdf(i, datasets[i])
 
 # Put together how many parts need to be retrieved. The min_qty is the minimum required to be pulled, which may result
 # in the same physical brick appearing in multiple images. The ideal_qty list is what's required to have unique physical
@@ -110,40 +245,6 @@ for part_number in scenario_pull_sheet.keys():
 
 # Now we've got the per-scenario, minimum, and ideal quantities for each part as well as a written description and
 # representative graphic for each part. Now make a PDF that we can print off and hold while pulling through Lego bins
-
-# Trying to get text formatted in the PDF was a bear; apparently there's no way to control the line spacing on a word-
-# wrapped cell using fpdf2 so eventually I gave up and wrote this function to manually word-wrap.
-def wrap_text_to_column_width(text:str, width: int) -> list[str]:
-    if len(text) <= width:
-        return [text]
-    split_idx = width-1
-    if text[split_idx] != ' ':
-        while split_idx >= 0:
-            if text[split_idx] == ' ':
-                break
-            split_idx -= 1
-    if split_idx < 0:
-        raise Exception(f'Failed to find a space that allows formatting text {text} to column width {width}')
-    # We're on a space now, so keep everything up to that space:
-    result = [text[:split_idx]]
-    # Recursively call to split the rest:
-    remainder = wrap_text_to_column_width(text[split_idx+1:], width)
-    # Join what we've got:
-    for item in remainder:
-        result.append(item)
-    # Return the result
-    return result
-
-
-# The following code based on Google's Generative AI in response to the search "create a table with images and save to
-# pdf python" on 30 October 2024.
-
-class PDF(FPDF):
-    def imagex(self, image_path, x, y, w=0, h=0):
-        if w == 0 and h == 0:
-            img = Image.open(image_path)
-            w, h = img.size
-        self.image(image_path, x, y, w, h)
 
 pdf = PDF()
 pdf.add_page()
